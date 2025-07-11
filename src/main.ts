43897baa47037461d7f055258b2fbbc15b903dd4
Benjamin <langlois.ben10@free.fr>
2025-07-11 12:27:29 +0200

    fix: main

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,6 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 void bootstrap();
