dd5355817ee4c0b7216837f6d3b7d1442e7a6aa9
Benjamin <langlois.ben10@free.fr>
2025-07-11 10:45:56 +0200

    fix

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,11 @@ import { join } from 'path';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors();
+  app.enableCors({
+    origin: ['http://localhost:5173', 'http://localhost:8081'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
